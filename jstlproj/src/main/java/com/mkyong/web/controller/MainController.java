8f7b8e035e25c5be77aaf5ead1055d1a4488f672
naveendhankher <naveen.dhankher@cognizant.com>
2016-01-08 11:37:47 +0530

    web app

---
+++ jstlproj/src/main/java/com/mkyong/web/controller/MainController.java
@@ -0,0 +1,41 @@
+package com.mkyong.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView getdata() {
+
+		List<String> list = getList();
+
+		ModelAndView model = new ModelAndView("index");
+		model.addObject("lists", list);
+
+		return model;
+
+	}
+
+	private List<String> getList() {
+
+		List<String> list = new ArrayList<String>();
+		list.add("List A");
+		list.add("List B");
+		list.add("List C");
+		list.add("List D");
+		list.add("List 1");
+		list.add("List 2");
+		list.add("List 3");
+
+		return list;
+
+	}
+
+}
